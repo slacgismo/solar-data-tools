ab647438964c4951d17bc79db9598f48d0390d78
Bennet Meyers <bennetm@stanford.edu>
2019-02-16 17:51:45 -0800

    initial commit

---
+++ solar-data-tools/clear_day_detection.py
@@ -0,0 +1,54 @@
+'''Clear Day Detection Module
+
+This module contains functions for detecting clear days in historical PV solar data sets.
+
+'''
+
+import numpy as np
+import cvxpy as cvx
+
+def clear_day_filter_full(data, th=0.1):
+    '''
+    This function quickly finds clear days in a PV power data set. The input to this function is a 2D array containing
+    standardized time series power data. This will typically be the output from
+    `solar-data-tools.data_transforms.make_2d`. The filter relies on two estimates of daily "clearness": the smoothness
+     of each daily signal as measured by the l2-norm of the 2nd order difference, and seasonally-adjusted daily
+     energy. Seasonal adjustment of the daily energy if obtained by solving a local quantile regression problem, which
+     is a convex optimization problem and is solvable with cvxpy. The parameter `th` controls the relative weighting of
+     the daily smoothness and daily energy in the final filter in a geometric mean. A value of 0 will rely entirely on
+     the daily energy and a value of 1 will rely entirely on daily smoothness.
+
+    :param D: A 2D numpy array containing a solar power time series signal.
+    :param th: A parameter that tunes the filter between relying of daily smoothness and daily energy
+    :return: A 1D boolean array, with `True` values corresponding to clear days in the data set
+    '''
+    # Take the norm of the second different of each day's signal. This gives a rough estimate of the smoothness of
+    # day in the data set
+    tc = np.linalg.norm(D[:-2] - 2 * D[1:-1] + D[2:], ord=1, axis=0)
+    # Shift this metric so the median is at zero
+    tc = np.percentile(tc, 50) - tc
+    # Normalize such that the maximum value is equal to one
+    tc /= np.max(tc)
+    # Take the positive part function, i.e. set the negative values to zero. This is the first metric
+    tc = np.clip(tc, 0, None)
+    # Calculate the daily energy
+    de = np.sum(D, axis=0)
+    # Solve a convex minimization problem to roughly fit the local 90th percentile of the data (quantile regression)
+    x = cvx.Variable(len(tc))
+    obj = cvx.Minimize(
+        cvx.sum(0.5 * cvx.abs(de - x) + (.9 - 0.5) * (de - x)) + 1e3 * cvx.norm(cvx.diff(x, k=2)))
+    prob = cvx.Problem(obj)
+    try:
+        prob.solve(solver='MOSEK')
+    except Exception as e:
+        print(e)
+        print('Trying ECOS solver')
+        prob.solve(solver='ECOS')
+    # x gives us the local top 90th percentile of daily energy, i.e. the very sunny days. This gives us our
+    # seasonal normalization.
+    de = np.clip(np.divide(de, x.value), 0, 1)
+    # Take geometric mean
+    weights = np.multiply(np.power(tc, th), np.power(de, 1.-th))
+    # Finally, set values less than 0.6 to be equal to zero
+    weights[weights < 0.6] = 0.
+    return weights >= 1e-3
---
+++ solar-data-tools/data_transforms.py
@@ -0,0 +1,59 @@
+''' Data Transforms Module
+
+This module contains functions for transforming PV power data, including time-axis standardization and
+2D-array generation
+
+'''
+
+from datetime import timedelta
+import numpy as np
+import pandas as pd
+
+def standardize_time_axis(df, datetimekey='Date-Time'):
+    '''
+    This function takes in a pandas data frame containing tabular time series data, likely generated with a call to
+    pandas.read_csv(). It is assumed that each row of the data frame corresponds to a unique date-time, though not
+    necessarily on standard intervals. This function will attempt to convert a user-specified column containing time
+    stamps to python datetime objects, assign this column to the index of the data frame, and then standardize the
+    index over time. By standardize, we mean reconstruct the index to be at regular intervals, starting at midnight of
+    the first day of the data set. This solves a couple common data errors when working with raw data. (1) Missing data
+    points from skipped scans in the data acquisition system. (2) Time stamps that are at irregular exact times,
+    including fractional seconds.
+
+    :param df: A pandas data frame containing the tabular time series data
+    :param datetimekey: An optional key corresponding to the name of the column that contains the time stamps
+    :return: A new data frame with a standardized time axis
+    '''
+    # convert index to timeseries
+    try:
+        df['Date-Time'] = pd.to_datetime(df['Date-Time'])
+        df.set_index('Date-Time', inplace=True)
+    except KeyError:
+        time_cols = [col for col in df.columns if np.logical_or('Time' in col, 'time' in col)]
+        key = time_cols[0]
+        df[key] = pd.to_datetime(df[key])
+        df.set_index(key, inplace=True)
+    # standardize the timeseries axis to a regular frequency over a full set of days
+    diff = (df.index[1:] - df.index[:-1]).seconds
+    freq = int(np.median(diff))  # the number of seconds between each measurement
+    start = df.index[0]
+    end = df.index[-1]
+    time_index = pd.date_range(start=start.date(), end=end.date() + timedelta(days=1), freq='{}s'.format(freq))[:-1]
+    df = df.reindex(index=time_index, method='nearest')
+    return df.fillna(value=0)
+
+def make_2d(df, key='dc_power'):
+    '''
+    This function constructs a 2D array (or matrix) from a time series signal with a standardized time axis. The data is
+    chunked into days, and each consecutive day becomes a column of the matrix.
+
+    :param df: A pandas data frame contained tabular data with a standardized time axis.
+    :param key: The key corresponding to the column in the data frame contained the signal to make into a matrix
+    :return: A 2D numpy array with shape (measurements per day, days in data set)
+    '''
+    if df is not None:
+        n_steps = int(24 * 60 * 60 / df.index.freq.n)
+        D = df[key].values.reshape(n_steps, -1, order='F')
+        return D
+    else:
+        return
