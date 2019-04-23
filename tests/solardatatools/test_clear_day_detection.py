1aac09612ccd5bcd7342f6431a5f8cc12275668e
Bennet Meyers <bennetm@stanford.edu>
2019-04-22 17:52:26 -0700

    moved test data for clear day detection into subfolder

--- tests/solardatatools/test_clear_day_detection.py
+++ tests/solardatatools/test_clear_day_detection.py
@@ -9,12 +9,12 @@ class TestClearDayDetection(unittest.TestCase):
 
         data_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
-            "../fixtures/one_year_power_signals_1.csv"))
+            "../fixtures/clear_day_detection/one_year_power_signals_1.csv"))
         with open(data_file_path) as file:
             data = np.loadtxt(file, delimiter=',')
         expected_data_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
-            "../fixtures/one_year_weights_1.csv"))
+            "../fixtures/clear_day_detection/one_year_weights_1.csv"))
         with open(expected_data_file_path) as file:
             expected_output = np.loadtxt(file, delimiter=',')
         expected_output = expected_output >= 1e-3
@@ -25,12 +25,12 @@ class TestClearDayDetection(unittest.TestCase):
     def test_clear_day_weights(self):
         data_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
-            "../fixtures/one_year_power_signals_1.csv"))
+            "../fixtures/clear_day_detection/one_year_power_signals_1.csv"))
         with open(data_file_path) as file:
             data = np.loadtxt(file, delimiter=',')
         expected_data_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
-            "../fixtures/one_year_weights_1.csv"))
+            "../fixtures/clear_day_detection/one_year_weights_1.csv"))
         with open(expected_data_file_path) as file:
             expected_output = np.loadtxt(file, delimiter=',')
         actual_output = find_clear_days(data, boolean_out=False)
