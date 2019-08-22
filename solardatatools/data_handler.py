a34afd20bdc566ee918647f1c7520562dda5b02c
Bennet Meyers <bennetm@stanford.edu>
2019-08-22 12:02:34 -0700

    adding clipping check, daily signals, and report method

--- solardatatools/data_handler.py
+++ solardatatools/data_handler.py
@@ -7,6 +7,8 @@ This module contains a class for managing a data processing pipeline
 
 import numpy as np
 from scipy.stats import mode
+from scipy.signal import argrelextrema
+from sklearn.neighbors.kde import KernelDensity
 import matplotlib.pyplot as plt
 from solardatatools.time_axis_manipulation import make_time_series,\
     standardize_time_axis, fix_time_shifts
@@ -22,6 +24,12 @@ class DataHandler():
                  convert_to_ts=False):
         self.data_frame = data_frame
         self.raw_data_matrix = raw_data_matrix
+        if self.raw_data_matrix is not None:
+            self.num_days = self.raw_data_matrix.shape[1]
+            self.data_sampling = int(24 * 60 / self.raw_data_matrix.shape[0])
+        else:
+            self.num_days = None
+            self.data_sampling = None
         self.filled_data_matrix = None
         self.keys = None
         self.use_column = None
@@ -29,13 +37,14 @@ class DataHandler():
         # Scores for the entire data set
         self.data_quality_score = None      # Fraction of days without data acquisition errors
         self.data_clearness_score = None    # Fraction of days that are approximately clear/sunny
+        # Flags for the entire data set
+        self.inverter_clipping = None       # True if there is inverter clipping, false otherwise
+        self.capacity_changes = None        # True if the apparent capacity seems to change over the data set
         # Daily scores (floats) and flags (booleans)
         self.daily_scores = DailyScores()
         self.daily_flags = DailyFlags()
         # Useful daily signals defined by the data set
-        self.daily_density_signal = None
-        self.seasonal_density_fit = None
-        self.daily_energy_signal = None
+        self.daily_signals = DailySignals()
         if np.alltrue([data_frame is not None, convert_to_ts]):
             df_ts, keys = make_time_series(self.data_frame)
             self.data_frame = df_ts
@@ -55,14 +64,31 @@ class DataHandler():
                              density_upper_threshold=density_upper_threshold,
                              linearity_threshold=linearity_threshold)
         self.detect_clear_days()
+        self.clipping_check()
         self.score_data_set()
         return
+
+    def report(self):
+        try:
+            l1 = 'Length:               {} days\n'.format(self.num_days)
+            l2 = 'Data sampling:        {} minute\n'.format(self.data_sampling)
+            l3 = 'Data quality score:   {:.1f}%\n'.format(self.data_quality_score * 100)
+            l4 = 'Data clearness score: {:.1f}%\n'.format(self.data_clearness_score * 100)
+            l5 = 'Inverter clipping:    {}'.format(self.inverter_clipping)
+            p_out = l1 + l2 + l3 + l4 + l5
+            print(p_out)
+            return
+        except TypeError:
+            print('Please run the pipeline first!')
+            return
 
     def make_data_matrix(self, use_col=None):
         df = standardize_time_axis(self.data_frame)
         if use_col is None:
             use_col = df.columns[0]
         self.raw_data_matrix = make_2d(df, key=use_col)
+        self.num_days = self.raw_data_matrix.shape[1]
+        self.data_sampling = int(24 * 60 / self.raw_data_matrix.shape[0])
         self.use_column = use_col
         return
 
@@ -75,7 +101,7 @@ class DataHandler():
         else:
             msk = np.isnan(self.filled_data_matrix)
             self.filled_data_matrix[msk] = 0
-        self.daily_energy_signal = np.sum(self.filled_data_matrix, axis=0) *\
+        self.daily_signals.energy = np.sum(self.filled_data_matrix, axis=0) *\
                                    24 / self.filled_data_matrix.shape[1]
         return
 
@@ -97,7 +123,7 @@ class DataHandler():
         if self.raw_data_matrix is None:
             print('Generate a raw data matrix first.')
             return
-        self.daily_scores.density, self.daily_density_signal, self.seasonal_density_fit\
+        self.daily_scores.density, self.daily_signals.density, self.daily_signals.seasonal_density_fit\
             = daily_missing_data_advanced(
             self.raw_data_matrix,
             threshold=threshold,
@@ -109,7 +135,7 @@ class DataHandler():
     def get_linearity_scores(self):
         if self.capacity_estimate is None:
             self.capacity_estimate = np.quantile(self.filled_data_matrix, 0.95)
-        if self.seasonal_density_fit is None:
+        if self.daily_signals.seasonal_density_fit is None:
             print('Run the density check first')
             return
         temp_mat = np.copy(self.filled_data_matrix)
@@ -117,7 +143,7 @@ class DataHandler():
         difference_mat = np.round(temp_mat[1:] - temp_mat[:-1], 4)
         modes, counts = mode(difference_mat, axis=0, nan_policy='omit')
         n = self.filled_data_matrix.shape[0] - 1
-        self.daily_scores.linearity = counts.data.squeeze() / (n * self.seasonal_density_fit)
+        self.daily_scores.linearity = counts.data.squeeze() / (n * self.daily_signals.seasonal_density_fit)
         return
 
     def score_data_set(self):
@@ -125,6 +151,42 @@ class DataHandler():
         self.data_quality_score = np.sum(self.daily_flags.no_errors) / num_days
         self.data_clearness_score = np.sum(self.daily_flags.clear) / num_days
         return
+
+    def clipping_check(self):
+        max_value = np.max(self.filled_data_matrix)
+        daily_max_val = np.max(self.filled_data_matrix, axis=0)
+        # 1st clipping statistic: ratio of the max value on each day to overall max value
+        clip_stat_1 = daily_max_val / max_value
+        # 2nd clipping statistic: fraction of time each day spent at that day's max value
+        with np.errstate(divide='ignore', invalid='ignore'):
+            temp = self.filled_data_matrix / daily_max_val
+            clip_stat_2 = np.sum(temp > 0.995, axis=0) / np.sum(temp > 0.005, axis=0)
+        clip_stat_2[np.isnan(clip_stat_2)] = 0
+        # Identify which days have clipping
+        clipped_days = np.logical_and(
+            clip_stat_1 > 0.05,
+            clip_stat_2 > 0.1
+        )
+        clipped_days = np.logical_and(
+            self.daily_flags.no_errors,
+            clipped_days
+        )
+        # clipped days must also be near a peak in the distribution of the
+        # 1st clipping statistic
+        peak_locs, peak_vals = self.__analyze_distribution(clip_stat_1)
+        clipped_days[clipped_days] = np.any(
+            np.array([np.abs(clip_stat_1[clipped_days] - x0) < .05 for x0 in
+                      peak_locs]), axis=0
+        )
+        self.daily_scores.clipping_1 = clip_stat_1
+        self.daily_scores.clipping_2 = clip_stat_2
+        self.daily_flags.inverter_clipped = clipped_days
+        if np.sum(clipped_days) > 0:
+            self.inverter_clipping = True
+        else:
+            self.inverter_clipping = False
+        return
+
 
     def auto_fix_time_shifts(self, c1=5., c2=500.):
         self.filled_data_matrix = fix_time_shifts(self.filled_data_matrix,
@@ -169,37 +231,37 @@ class DataHandler():
             return fig
 
     def plot_density_signal(self, flag=None, show_fit=False, figsize=(8, 6)):
-        if self.daily_density_signal is None:
+        if self.daily_signals.density is None:
             return
         fig = plt.figure(figsize=figsize)
-        plt.plot(self.daily_density_signal, linewidth=1)
-        xs = np.arange(len(self.daily_density_signal))
+        plt.plot(self.daily_signals.density, linewidth=1)
+        xs = np.arange(len(self.daily_signals.density))
         title = 'Daily signal density'
         if flag == 'good':
             plt.scatter(xs[self.daily_flags.no_errors],
-                        self.daily_density_signal[self.daily_flags.no_errors],
+                        self.daily_signals.density[self.daily_flags.no_errors],
                         color='red')
             title += ', good days flagged'
         elif flag == 'bad':
             plt.scatter(xs[~self.daily_flags.no_errors],
-                        self.daily_density_signal[~self.daily_flags.no_errors],
+                        self.daily_signals.density[~self.daily_flags.no_errors],
                         color='red')
             title += ', bad days flagged'
         elif flag in ['clear', 'sunny']:
             plt.scatter(xs[self.daily_flags.clear],
-                        self.daily_density_signal[self.daily_flags.clear],
+                        self.daily_signals.density[self.daily_flags.clear],
                         color='red')
             title += ', clear days flagged'
         elif flag == 'cloudy':
             plt.scatter(xs[self.daily_flags.cloudy],
-                        self.daily_density_signal[self.daily_flags.cloudy],
+                        self.daily_signals.density[self.daily_flags.cloudy],
                         color='red')
             title += ', cloudy days flagged'
-        if np.logical_and(show_fit, self.seasonal_density_fit is not None):
-            plt.plot(self.seasonal_density_fit, color='orange')
-            plt.plot(0.6 * self.seasonal_density_fit, color='green', linewidth=1,
+        if np.logical_and(show_fit, self.daily_signals.seasonal_density_fit is not None):
+            plt.plot(self.daily_signals.seasonal_density_fit, color='orange')
+            plt.plot(0.6 * self.daily_signals.seasonal_density_fit, color='green', linewidth=1,
                      ls='--')
-            plt.plot(1.05 * self.seasonal_density_fit, color='green', linewidth=1,
+            plt.plot(1.05 * self.daily_signals.seasonal_density_fit, color='green', linewidth=1,
                      ls='--')
         plt.title(title)
         return fig
@@ -209,7 +271,7 @@ class DataHandler():
         if self.filled_data_matrix is None:
             return
         fig = plt.figure(figsize=figsize)
-        energy = self.daily_energy_signal
+        energy = self.daily_signals.energy
         plt.plot(energy, linewidth=1)
         xs = np.arange(len(energy))
         title = 'Daily energy production'
@@ -236,11 +298,108 @@ class DataHandler():
         plt.title(title)
         return fig
 
+    def plot_clipping(self, figsize=(10, 8)):
+        if self.daily_scores.clipping_1 is None:
+            return
+        fig, ax = plt.subplots(nrows=2, figsize=figsize, sharex=True)
+        clip_stat_1 = self.daily_scores.clipping_1
+        clip_stat_2 = self.daily_scores.clipping_2
+        clipped_days = self.daily_flags.inverter_clipped
+        ax[0].plot(clip_stat_1)
+        ax[1].plot(clip_stat_2)
+        ax[0].scatter(np.arange(len(clip_stat_1))[clipped_days],
+                      clip_stat_1[clipped_days], color='red', label='days with inverter clipping')
+        ax[1].scatter(np.arange(len(clip_stat_2))[clipped_days],
+                      clip_stat_2[clipped_days], color='red')
+        ax[0].set_title('Clipping Score 1: ratio of daily max to overal max')
+        ax[1].set_title('Clipping Score 2: fraction of time each day spent at daily max')
+        ax[0].legend()
+        return fig
+
+    def __analyze_distribution(self, data, plot=False):
+        # set the bandwidth for the KDE algorithm dynamically as a logarithmic
+        # function of the number of values. The function roughly follows the
+        # following:
+        #
+        # data length  |  bandwidth
+        # -------------|------------
+        #     10       |     0.08
+        #     50       |     0.05
+        #     500      |     0.025
+        #     2000     |     0.01
+        coeffs = np.array(
+            [1.28782573e+01 / 1000, 2.99960708e-07, -8.76881301e+01 / 1000])
+
+        def bdw(x):
+            out = coeffs[0] * -np.log(coeffs[1] * x) + coeffs[2]
+            return np.clip(out, 0.01, 0.1)
+
+        bandwidth = bdw(len(data))
+
+        kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(
+            data[data > 0][:, np.newaxis])
+        X_plot = np.linspace(np.min(data) - 0.01,
+                             np.max(data) + 0.01)[:, np.newaxis]
+        log_dens = kde.score_samples(X_plot)
+        mins = argrelextrema(log_dens, np.less)[0]  # potential cut points to make clusters
+        maxs = argrelextrema(log_dens, np.greater)[0]  # locations of the max point in each cluster
+        if len(mins) >= len(maxs):
+            if mins[0] < maxs[0]:
+                mins = mins[1:]
+            if mins[-1] > maxs[-1]:
+                mins = mins[:-1]
+
+        keep_mxs = np.ones_like(maxs, dtype=np.bool)
+        keep_mns = np.ones_like(mins, dtype=np.bool)
+        index = np.arange(len(maxs))
+        done = False
+        if len(mins) > 0:
+            while not done:
+                comp_left = np.exp(log_dens[maxs[keep_mxs][:-1]]) - np.exp(
+                    log_dens[mins[keep_mns]])
+                comp_right = np.exp(log_dens[maxs[keep_mxs][1:]]) - np.exp(
+                    log_dens[mins[keep_mns]])
+                comp_array = np.c_[comp_left, comp_right]
+                min_diff = comp_array.min()
+                if min_diff < 0.35:
+                    min_dif_loc = np.unravel_index(comp_array.argmin(),
+                                                   comp_array.shape)
+                    drop_min = min_dif_loc[0]
+                    keep_mns[mins == mins[keep_mns][drop_min]] = 0
+                    if np.exp(log_dens[maxs[keep_mxs][drop_min]]) > np.exp(
+                            log_dens[maxs[keep_mxs][drop_min + 1]]):
+                        keep_mxs[maxs == maxs[keep_mxs][drop_min + 1]] = 0
+                    else:
+                        keep_mxs[maxs == maxs[keep_mxs][drop_min]] = 0
+                else:
+                    done = True
+                if np.sum(keep_mns) == 0:
+                    done = True
+
+        if plot:
+            plt.hist(data, bins=100)
+            plt.plot(X_plot.squeeze(),
+                     0.01 * len(data) * np.exp(log_dens))
+            for mn in mins[keep_mns]:
+                plt.axvline(X_plot[:, 0][mn], linewidth=1, linestyle=':',
+                            color='green')
+            for mx in maxs[keep_mxs]:
+                plt.axvline(X_plot[:, 0][mx], linewidth=1, linestyle='--',
+                            color='red')
+            fig = plt.gcf()
+            return fig
+        else:
+            peak_locs = X_plot[:, 0][maxs[keep_mxs]]
+            peak_vals = np.exp(kde.score_samples(peak_locs[:, np.newaxis]))
+            return peak_locs, peak_vals
+
 
 class DailyScores():
     def __init__(self):
         self.density = None
         self.linearity = None
+        self.clipping_1 = None
+        self.clipping_2 = None
 
 
 class DailyFlags():
@@ -250,6 +409,7 @@ class DailyFlags():
         self.no_errors = None
         self.clear = None
         self.cloudy = None
+        self.inverter_clipped = None
 
     def flag_no_errors(self):
         self.no_errors = np.logical_and(self.density, self.linearity)
@@ -257,3 +417,9 @@ class DailyFlags():
     def flag_clear_cloudy(self, clear_days):
         self.clear = np.logical_and(clear_days, self.no_errors)
         self.cloudy = np.logical_and(~self.clear, self.no_errors)
+
+class DailySignals():
+    def __init__(self):
+        self.density = None
+        self.seasonal_density_fit = None
+        self.energy = None
