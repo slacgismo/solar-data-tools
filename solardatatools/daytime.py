002cb2846d800c882c2547f807937ed47b7fdd2c
Bennet Meyers <bennetm@stanford.edu>
2020-04-27 15:21:33 -0700

    documentation

--- solardatatools/daytime.py
+++ solardatatools/daytime.py
@@ -8,9 +8,30 @@ series
 import numpy as np
 
 def find_daytime(data_matrix, threshold=0.01):
+    """
+    Function for creating a boolean mask of time periods when the sun is
+    up on a PV system, based on a a power signal. The data is scaled so that
+    "night time" data is approximately zero and the maximum power output
+    approximately 1. We take the 5th and 95th percentiles for the top and
+    bottom scale factors instead of using the maximum and the minimum. This
+    provides robustness again outlier measurements. Then, daytime is chosen
+    as scaled values larger than the threshold, which has a default value of
+    0.01, i.e. power measurements larger than 1% of the 95th percentile. If
+    the 95th percentile corresponds to "one sun" of irradiance or 1000 W/m^2,
+    this default threshold value represents irradiance levels above 10 W/m^2.
+
+    :param data_matrix: A power data matrix, possibly with NaN values
+    :param threshold: daytime threshold
+    :return: boolean mask of daytime periods, matching the power matrix
+    """
     mat_copy = np.copy(data_matrix)
+    # convert NaNs to zeros
     np.nan_to_num(mat_copy, copy=False, nan=0.0)
-    mat_copy -= np.quantile(mat_copy, 0.05)
-    mat_copy /= np.max(mat_copy)
+    # scale mat_copy to mostly have values >=0 and <= 1, allowing for outliers
+    bottom_scale = np.quantile(mat_copy, 0.05)
+    top_scale = np.quantile(mat_copy, 0.95)
+    mat_copy -= bottom_scale
+    mat_copy /= top_scale - bottom_scale
+    # define boolean mask rule
     daytime_mask = mat_copy >= threshold
     return daytime_mask
