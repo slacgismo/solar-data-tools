f21f21aa9ab4e31e3fe660b3d2f306e20dc5f226
Bennet Meyers <bennetm@stanford.edu>
2020-10-08 16:43:02 -0700

    simplify nighttime fill procedure using new sunrise/sunset class

--- solardatatools/data_filling.py
+++ solardatatools/data_filling.py
@@ -9,37 +9,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from solardatatools.daytime import find_daytime
+from solardatatools.algorithms import SunriseSunset
 
-def zero_nighttime(data_matrix):
+def zero_nighttime(data_matrix, night_mask=None, daytime_threshold=0.005):
     D = np.copy(data_matrix)
-    night_msk = ~find_daytime(D)
-    # mat_copy = np.copy(D)
-    # np.nan_to_num(mat_copy, copy=False, nan=0.0)
-    # mat_copy -= np.quantile(mat_copy, 0.05)
-    # mat_copy /= np.max(mat_copy)
-    # night_msk = mat_copy <= 0.01
-    ### Debugging
-    # plt.imshow(night_msk, interpolation='none', aspect='auto')
-    # plt.colorbar()
-    # plt.show()
-    ### Old code
-    # try:
-    #     with np.errstate(invalid='ignore'):
-    #         night_msk = D < 0.005 * np.max(D[~np.isnan(D)])
-    # except ValueError:
-    #     night_msk = D < 0.005 * np.max(D)
-    D[night_msk] = np.nan
-    good_vals = (~np.isnan(D)).astype(int)
-    sunrise_idxs = np.argmax(good_vals, axis=0)
-    sunset_idxs = D.shape[0] - np.argmax(np.flip(good_vals, 0), axis=0)
-    D_msk = np.zeros_like(D, dtype=np.bool)
-    for ix in range(D.shape[1]):
-        if sunrise_idxs[ix] > 0:
-            D_msk[:sunrise_idxs[ix] - 1, ix] = True
-            D_msk[sunset_idxs[ix] + 1:, ix] = True
-        else:
-            D_msk[:, ix] = True
-    D[D_msk] = 0
+    if night_mask is None:
+        ss = SunriseSunset()
+        ss.calculate_times(data_matrix, threshold=daytime_threshold)
+        night_mask = ~ss.sunup_mask_estimated
+    D[night_mask] = 0
     return D
 
 def interp_missing(data_matrix):
