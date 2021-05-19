842756393e64a8221ee421065e6f62113b38d37b
Bennet Meyers <bennetm@stanford.edu>
2021-05-18 17:08:24 -0700

    we do not need to expose the solver yet!

--- solardatatools/data_quality.py
+++ solardatatools/data_quality.py
@@ -51,8 +51,7 @@ def daily_missing_data_advanced(data_matrix, threshold=0.2,
     fit_signal = local_quantile_regression_with_seasonal(
         density_signal,
         use_ixs=use_days,
-        tau=0.85,
-        solver=solver
+        tau=0.85
     )
     scores = density_signal / fit_signal
     out = [scores]
