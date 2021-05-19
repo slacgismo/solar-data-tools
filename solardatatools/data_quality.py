9ed8524d025c71fa42ae336ea4c9a45f33b4e527
Bennet Meyers <bennetm@stanford.edu>
2021-05-18 17:27:09 -0700

    expose solver kwarg again

--- solardatatools/data_quality.py
+++ solardatatools/data_quality.py
@@ -50,7 +50,8 @@ def daily_missing_data_advanced(data_matrix, threshold=0.2,
     fit_signal = local_quantile_regression_with_seasonal(
         density_signal,
         use_ixs=use_days,
-        tau=0.85
+        tau=0.85,
+        solver=solver
     )
     scores = density_signal / fit_signal
     out = [scores]
