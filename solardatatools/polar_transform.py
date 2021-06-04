b2827d0eafbf1a20bb8c9b91a75359835de7cfaa
Bennet Meyers <bennetm@stanford.edu>
2021-06-04 14:05:38 -0700

    annotations

--- solardatatools/polar_transform.py
+++ solardatatools/polar_transform.py
@@ -55,22 +55,38 @@ class PolarTransform():
 
     def transform(self, elevation_round=1, azimuth_round=2,
                   agg_func=np.nanmean):
+        # Solar position subroutine requires TZ-aware time stamps or UTC time
+        # stamps, but we typically have non-TZ-aware local time, without DST
+        # shifts (which is the most natural for solar data). So, step 1 is to
+        # convert the time to UTC.
         times = self.data.index.shift(-self.tz_offset, 'H')
+        # run solar position subroutine
         solpos = get_solarposition(times, self.lat, self.lon)
+        # reset the time from UTC to local
         solpos.index = self.data.index
+        # join the calculated angles with the power measurments
         triples = solpos[['apparent_elevation', 'azimuth']].join(
             self.normed_data.loc[self.bix])
         triples = triples.dropna()
+        # cut off all the entries corresponding to the sun below the horizon
         triples = triples[triples['apparent_elevation'] >= 0]
+        # a function for rounding to the nearest integer c (e.g. 2, 5, 10...)
         my_round = lambda x, c: c * np.round(x / c, 0)
+        # create elevation and azimuth bins
         triples['elevation_angle'] = my_round(triples['apparent_elevation'],
                                           elevation_round)
         triples['azimuth_angle'] = my_round(triples['azimuth'],
                                         azimuth_round)
+        # group by bins
         grouped = triples.groupby(['azimuth_angle', 'elevation_angle'])
+        # calculation aggregation function for each bin
         grouped = grouped.agg(agg_func)
+        # remove other columns
         grouped = grouped['normalized-power']
+        # convert tall data format to wide data format, fill misisng values
+        # with zero, and transpose
         estimates = grouped.unstack().fillna(0).T
+        # reverse the first axis
         estimates = estimates.iloc[::-1]
         self.tranformed_data = estimates
 
