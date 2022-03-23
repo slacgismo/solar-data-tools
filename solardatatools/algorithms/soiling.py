a75d058798a25659dc0a8d6aaf6da1d8ff978337
Bennet Meyers <bennetm@stanford.edu>
2022-03-23 14:03:05 -0700

    changing default behavior

--- solardatatools/algorithms/soiling.py
+++ solardatatools/algorithms/soiling.py
@@ -10,20 +10,22 @@ import numpy as np
 import cvxpy as cvx
 
 DEFAULT = {
-    'degradation_term': True,
-    'tau': 0.85,
-    'iterations': 5,
-    'w1': 2,
-    'w2': 3e-2,
-    'w3': 2e-1,
-    'w4': 5e2,
+    "degradation_term": True,
+    "tau": 0.85,
+    "iterations": 5,
+    "w1": 2,
+    "w2": 3e-2,
+    "w3": 2e-1,
+    "w4": 5e2,
 }
+
 
 class SoilingAnalysis:
     def __init__(self, data_handler):
         self.dh = data_handler
-        e1 = np.nansum(self.dh.raw_data_matrix, axis=0)
-        self.soiling_signal = e1 / np.nanquantile(e1, .98)
+        e1 = data_handler.daily_signals.energy
+        self.scaling = np.nanquantile(e1, 0.98)
+        self.soiling_signal = e1 / self.scaling
         self.soiling_component = None
         self.seasonal_component = None
         self.degradation_component = None
@@ -35,9 +37,7 @@ class SoilingAnalysis:
         if len(config) == 0:
             config = DEFAULT
         s1e, s2e, s3e, sre = soiling_seperation(
-            self.soiling_signal,
-            index_set=self.dh.daily_flags.no_errors,
-            **config
+            self.soiling_signal, index_set=self.dh.daily_flags.no_errors, **config
         )
         self.soiling_component = s1e
         self.seasonal_component = s2e
@@ -45,13 +45,24 @@ class SoilingAnalysis:
         self.residual_component = sre
         self.correction_factor = 1 + s1e
         self.corr_raw_matrix = self.dh.raw_data_matrix / self.correction_factor
-        self.corr_filled_data_matrix = (self.dh.filled_data_matrix
-                                        / self.correction_factor)
+        self.corr_filled_data_matrix = (
+            self.dh.filled_data_matrix / self.correction_factor
+        )
 
 
-def soiling_seperation(observed, index_set=None, degradation_term=False,
-                       period=365, tau=0.85, w1=2, w2=1e-2, w3=1e0, w4=1e2,
-                       iterations=5, solver='MOSEK'):
+def soiling_seperation(
+    observed,
+    index_set=None,
+    degradation_term=True,
+    tau=0.85,
+    w1=2,
+    w2=3e-2,
+    w3=2e-1,
+    w4=5e2,
+    iterations=5,
+    solver="MOSEK",
+    period=365,
+):
     """
     Apply signal decomposition framework to Performance Index soiling estimation
     problem. The PI signal is a daily performance index, typically daily energy
@@ -77,12 +88,12 @@ def soiling_seperation(observed, index_set=None, degradation_term=False,
     else:
         index_set = np.logical_and(index_set, ~np.isnan(observed))
     # zero_set = np.zeros(len(observed) - 1, dtype=np.bool)
-    eps = .01
+    eps = 0.01
     n = len(observed)
-    s1 = cvx.Variable(n)            # soiling
+    s1 = cvx.Variable(n)  # soiling
     s2 = cvx.Variable(max(n, 367))  # seasonal
-    s3 = cvx.Variable(n)            # degradation
-    sr = cvx.Variable(n)            # residual
+    s3 = cvx.Variable(n)  # degradation
+    sr = cvx.Variable(n)  # residual
     # z = cvx.Variable(2)
     # T = len(observed)
     # M = np.c_[np.ones(T), np.arange(T)]
@@ -91,28 +102,30 @@ def soiling_seperation(observed, index_set=None, degradation_term=False,
 
     # cvx.norm(cvx.multiply(s3, weights), p=2) \
 
-    cost = cvx.sum(tau * cvx.pos(sr) +(1 - tau) * cvx.neg(sr)) \
-           + w4 * cvx.norm(cvx.diff(s2[:n], k=2), p=2) \
-           + w1 * cvx.norm(cvx.multiply(w, cvx.diff(s1, k=2)), p=1) \
-           + w2 * cvx.sum(-s1) \
-           + w3 * (cvx.sum(0.9 * cvx.pos(cvx.diff(s1, k=1))
-                           + (1 - 0.1) * cvx.neg(cvx.diff(s1, k=1))))
+    cost = (
+        cvx.sum(tau * cvx.pos(sr) + (1 - tau) * cvx.neg(sr))
+        + w4 * cvx.norm(cvx.diff(s2[:n], k=2), p=2)
+        + w1 * cvx.norm(cvx.multiply(w, cvx.diff(s1, k=2)), p=1)
+        + w2 * cvx.sum(-s1)
+        + w3
+        * (
+            cvx.sum(
+                0.9 * cvx.pos(cvx.diff(s1, k=1))
+                + (1 - 0.1) * cvx.neg(cvx.diff(s1, k=1))
+            )
+        )
+    )
     objective = cvx.Minimize(cost)
     constraints = [
         observed[index_set] == (s1 + s2[:n] + s3 + sr)[index_set],
         s2[period:] - s2[:-period] == 0,
         # cvx.sum(s2[:period]) == 0,
-        s1 <= 0
+        s1 <= 0,
     ]
     if degradation_term:
-        constraints.extend([
-            cvx.diff(s3, k=2) == 0,
-            s3[0] == 0
-        ])
+        constraints.extend([cvx.diff(s3, k=2) == 0, s3[0] == 0])
     else:
-        constraints.append(
-            s3 == 0
-        )
+        constraints.append(s3 == 0)
     # if np.sum(zero_set) > 0:
     #     constraints.append(cvx.diff(s1, k=1)[zero_set] == 0)
     if n < 0.75 * 365:
@@ -120,14 +133,26 @@ def soiling_seperation(observed, index_set=None, degradation_term=False,
     problem = cvx.Problem(objective, constraints)
     for i in range(iterations):
         problem.solve(solver=solver)
-        w.value = 1 / (eps + 1e2* np.abs(cvx.diff(s1, k=2).value))   # Reweight the L1 penalty
+        w.value = 1 / (
+            eps + 1e2 * np.abs(cvx.diff(s1, k=2).value)
+        )  # Reweight the L1 penalty
         # zero_set = np.abs(cvx.diff(s1, k=1).value) <= 5e-5     # Make nearly flat regions exactly flat (sparse 1st diff)
     return s1.value, s2.value[:n], s3.value, sr.value
 
 
-def soiling_seperation_old(observed, index_set=None, degradation_term=False,
-                           period=365, tau=0.85, w1=2, w2=1e-2, w3=100,
-                           iterations=5, soiling_max=1.0, solver='MOSEK'):
+def soiling_seperation_old(
+    observed,
+    index_set=None,
+    degradation_term=False,
+    period=365,
+    tau=0.85,
+    w1=2,
+    w2=1e-2,
+    w3=100,
+    iterations=5,
+    soiling_max=1.0,
+    solver="MOSEK",
+):
     """
     Apply signal decomposition framework to Performance Index soiling estimation
     problem. The PI signal is a daily performance index, typically daily energy
@@ -153,12 +178,12 @@ def soiling_seperation_old(observed, index_set=None, degradation_term=False,
     else:
         index_set = np.logical_and(index_set, ~np.isnan(observed))
     # zero_set = np.zeros(len(observed) - 1, dtype=np.bool)
-    eps = .01
+    eps = 0.01
     n = len(observed)
-    s1 = cvx.Variable(n)            # soiling
+    s1 = cvx.Variable(n)  # soiling
     s2 = cvx.Variable(max(n, 367))  # seasonal
-    s3 = cvx.Variable(n)            # degradation
-    sr = cvx.Variable(n)            # residual
+    s3 = cvx.Variable(n)  # degradation
+    sr = cvx.Variable(n)  # residual
     # z = cvx.Variable(2)
     # T = len(observed)
     # M = np.c_[np.ones(T), np.arange(T)]
@@ -167,26 +192,23 @@ def soiling_seperation_old(observed, index_set=None, degradation_term=False,
 
     # cvx.norm(cvx.multiply(s3, weights), p=2) \
 
-    cost = cvx.sum(tau * cvx.pos(sr) +(1 - tau) * cvx.neg(sr)) \
-           + w3 * cvx.norm(cvx.diff(s2[:n], k=2), p=2) \
-           + w1 * cvx.norm(cvx.multiply(w, cvx.diff(s1, k=2)), p=1) \
-           + w2 * cvx.sum(soiling_max - s1)
+    cost = (
+        cvx.sum(tau * cvx.pos(sr) + (1 - tau) * cvx.neg(sr))
+        + w3 * cvx.norm(cvx.diff(s2[:n], k=2), p=2)
+        + w1 * cvx.norm(cvx.multiply(w, cvx.diff(s1, k=2)), p=1)
+        + w2 * cvx.sum(soiling_max - s1)
+    )
     objective = cvx.Minimize(cost)
     constraints = [
         observed[index_set] == (s1 + s2[:n] + s3 + sr)[index_set],
         s2[period:] - s2[:-period] == 0,
         cvx.sum(s2[:period]) == 0,
-        s1 <= soiling_max
+        s1 <= soiling_max,
     ]
     if degradation_term:
-        constraints.extend([
-            cvx.diff(s3, k=2) == 0,
-            s3[0] == 0
-        ])
+        constraints.extend([cvx.diff(s3, k=2) == 0, s3[0] == 0])
     else:
-        constraints.append(
-            s3 == 0
-        )
+        constraints.append(s3 == 0)
     # if np.sum(zero_set) > 0:
     #     constraints.append(cvx.diff(s1, k=1)[zero_set] == 0)
     if n < 0.75 * 365:
@@ -194,6 +216,8 @@ def soiling_seperation_old(observed, index_set=None, degradation_term=False,
     problem = cvx.Problem(objective, constraints)
     for i in range(iterations):
         problem.solve(solver=solver)
-        w.value = 1 / (eps + 1e2* np.abs(cvx.diff(s1, k=2).value))   # Reweight the L1 penalty
+        w.value = 1 / (
+            eps + 1e2 * np.abs(cvx.diff(s1, k=2).value)
+        )  # Reweight the L1 penalty
         # zero_set = np.abs(cvx.diff(s1, k=1).value) <= 5e-5     # Make nearly flat regions exactly flat (sparse 1st diff)
     return s1.value, s2.value[:n], s3.value, sr.value
