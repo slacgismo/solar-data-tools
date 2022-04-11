0f77ee3e67f16199fd9043decf27ef48dce20a70
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2022-04-11 14:37:54 -0700

    Correct syntax

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from io import open
 here = Path()
 
 # Get the long description from the README file
-with open((here / "README.md"), "README.md"), encoding="utf-8") as f:
+with open((here / "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # get all the git tags from the cmd line that follow our versioning pattern
@@ -40,7 +40,7 @@ latest_version = latest_git_tag.communicate()[0]
 # PEP 440 won't accept the v in front, so here we remove it, strip the new line and decode the byte stream
 VERSION_FROM_GIT_TAG = latest_version[1:].strip().decode("utf-8")
 
-with open((here / "README.md"), "requirements.txt"), encoding="utf-8") as f:
+with open((here / "requirements.txt"), encoding="utf-8") as f:
     install_requires = f.read().splitlines()
 # removes comments in the requirements file
 dependencies = [dependency for dependency in install_requires if (dependency[0] != "#")]
