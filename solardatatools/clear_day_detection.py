d5e0e5579a8a1d69b25e655f905f5e835233c4de
Sara A. Miskovich <saramiskovich@gmail.com>
2023-05-11 16:36:02 -0700

    simplified and adjusted docstrings

--- solardatatools/clear_day_detection.py
+++ solardatatools/clear_day_detection.py
@@ -28,10 +28,10 @@ class ClearDayDetection:
             solver="ECOS"
     ):
         capacity_est = np.nanquantile(data, 0.95)
-        foo = data_copy > 0.02 * capacity_est  # 2% of 95th perc
         # set nans to zero to avoid issues w/ summing
         data_copy = np.copy(data)
         data_copy[np.isnan(data)] = 0.0
+        foo = data_copy > 0.02 * capacity_est  # 2% of 95th perc
         self.density_signal = np.sum(foo, axis=0) / data.shape[0]
         use_days = np.logical_and(self.density_signal > 0.2, self.density_signal < 0.8)
 
