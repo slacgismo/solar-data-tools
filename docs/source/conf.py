096b07cb5f5586cba25c57f867d8748baedeeb9c
Sara A. Miskovich <saramiskovich@gmail.com>
2024-01-04 17:27:00 -0800

    added sphinx extension and theme info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sphinx_rtd_theme
+import pydata_sphinx_theme
 import os
 import sys
 
