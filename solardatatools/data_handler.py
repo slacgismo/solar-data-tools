d5a2f4ffda9feab4b76e2b8c30bed5d1e510b218
Sara A. Miskovich <saramiskovich@gmail.com>
2024-03-21 15:22:59 -0700

    test DH

--- solardatatools/data_handler.py
+++ solardatatools/data_handler.py
@@ -46,7 +46,7 @@ register_matplotlib_converters()
 from solardatatools.polar_transform import PolarTransform
 
 
-class DataHandler:
+class DataHandlers:
     def __init__(
         self,
         data_frame=None,
