62fe0e768371444fa384255bc5e07d2a72700ddf
nimishy <114829354+nimishy@users.noreply.github.com>
2024-04-18 21:53:16 -0400

    Implemented Logging and handles multiprocessing logs, and timestamps

--- sdt_dask/dask_tool/sdt_dask.py
+++ sdt_dask/dask_tool/sdt_dask.py
@@ -7,16 +7,34 @@ See the README and tool_demo_SDTDask.ipynb for more information
 
 """
 import os
+import sys
+import logging
 import pandas as pd
+from time import strftime
 from dask import delayed
 from dask.distributed import performance_report
+from distributed.worker import logger
 from solardatatools import DataHandler
+
+# logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:'
+                               '%(module)s: %(message)s')
+os.makedirs("../logs/", exist_ok=True)
+file_handler = logging.FileHandler('../logs/sdt_dask.log')
+stream_handler = logging.StreamHandler(sys.stdout)
+
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 class SDTDask:
     """A class to run the SolarDataTools pipeline on a Dask cluster.
         Will handle invalid data keys and failed datasets.
 
-        :param keys: 
+        :param keys:
             data_plug (:obj:`DataPlug`): The data plug object.
             client (:obj:`Client`): The Dask client object.
             output_path (str): The path to save the results.
@@ -26,7 +44,7 @@ class SDTDask:
         self.data_plug = data_plug
         self.client = client
         self.output_path = output_path
-        
+
     def set_up(self, KEYS, **kwargs):
         """function to set up the pipeline on the data plug
 
@@ -38,13 +56,14 @@ class SDTDask:
             **kwargs: Optional parameters.
 
         """
+        logger.info(" Setting up SDT dask tool")
 
         reports = []
         runtimes = []
         losses = []
         get_data_errors = []
         errors = []
-    
+
         class Data:
             reports = None
             loss_reports = None
@@ -56,7 +75,7 @@ class SDTDask:
                 self.runtime = runtime
                 self.errors = errors
 
-        
+
         class DaskErrors:
             run_pipeline_errors = None
             run_pipeline_report_errors = None
@@ -76,26 +95,37 @@ class SDTDask:
 
         def run(datahandler, **kwargs):
             run_pipeline_error = None
+
             if datahandler is None:
+                logger.error(f'{str(datahandler)[-19:-1]} : value is None')
                 return None
             try:
                 datahandler.run_pipeline(**kwargs)
+                logger.info(f'{str(datahandler)[-19:-1]} : Runnning Pipeline..')
                 if datahandler.num_days <= 365:
                     datahandler.run_loss_analysis_error = "The length of data is less than or equal to 1 year, loss analysis will fail thus is not performed."
                     datahandler.loss_analysis_report_error = "Loss analysis is not performed"
+                    logger.warning(f"{str(datahandler)[-19:-1]} : Cannot perform loss "
+                                   "analysis as the duration of the datahandler "
+                                   "is less than a year")
                     return datahandler
             except Exception as e:
                 datahandler.run_pipeline_error = str(e)
                 datahandler.run_loss_analysis_error = "Failed because of run_pipeline error"
                 datahandler.run_pipeline_report_error = "Failed because of run_pipeline error"
                 datahandler.loss_analysis_report_error = "Failed because of run_pipeline error"
+                logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
                 return datahandler
 
-            try: 
+            try:
                 datahandler.run_loss_factor_analysis()
-            except Exception as e:  
+                logger.info(f"{str(datahandler)[-19:-1]} : Running loss "
+                            f"factor analysis ...")
+            except Exception as e:
                 datahandler.run_loss_analysis_error = str(e)
                 datahandler.loss_analysis_report_error = "Failed because of run_loss_analysis error"
+                logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
+
                 return datahandler
 
             return datahandler
@@ -110,35 +140,47 @@ class SDTDask:
             loss_analysis_report_error = None
             if datahandler is None:
                 errors = DaskErrors("get_data error leading nothing to run", "get_data error leading nothing to analyze", "get_data error leading nothing to report", "get_data error leading nothing to report")
+                logger.error(f"{str(datahandler)[-19:-1]}: Datahandler is none")
                 return Data(report, loss_report, runtime, errors)
-            
+
             if hasattr(datahandler, "run_pipeline_error"):
                 run_pipeline_error = datahandler.run_pipeline_error
+                logger.error(f"{str(datahandler)[-19:-1]} : {run_pipeline_error}")
             else:
                 try:
                     report = datahandler.report(return_values=True, verbose=False)
+                    logger.info(f"{str(datahandler)[-19:-1]} : Getting "
+                                f"Datahandler Report ...")
                 except Exception as e:
                     datahandler.run_pipeline_report_error = str(e)
-            
+                    logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
+
             if hasattr(datahandler, "run_loss_analysis_error"):
                 run_loss_analysis_error = datahandler.run_loss_analysis_error
+                logger.error(f"{str(datahandler)[-19:-1]} : {run_loss_analysis_error}")
             else:
                 try:
                     loss_report = datahandler.loss_analysis.report()
+                    logger.info(f"{str(datahandler)[-19:-1]} : Getting Loss "
+                                f"analysis Report ...")
                 except Exception as e:
                     datahandler.loss_analysis_report_error = str(e)
+                    logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
 
             try:
                 runtime = datahandler.total_time
+                logger.info(f"{str(datahandler)[-19:-1]} : Getting Total Time ...")
             except Exception as e:
-                print(e)
-            
+                logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
+
             if hasattr(datahandler, "run_pipeline_report_error"):
                 run_pipeline_report_error = datahandler.run_pipeline_report_error
+                logger.error(f"{str(datahandler)[-19:-1]} : {run_pipeline_report_error}")
 
             if hasattr(datahandler, "loss_analysis_report_error"):
                 loss_analysis_report_error = datahandler.loss_analysis_report_error
-            
+                logger.error(f"{str(datahandler)[-19:-1]} : {loss_analysis_report_error}")
+
             errors = DaskErrors(run_pipeline_error, run_loss_analysis_error, run_pipeline_report_error, loss_analysis_report_error)
             return Data(report, loss_report, runtime, errors)
 
@@ -151,7 +193,7 @@ class SDTDask:
 
         def helper_data(datas):
             return [data if data is not None else {} for data in datas]
-        
+
         def generate_errors(errors):
             # input is a list of Errors objects
             # output is a attribute-list dictionary
@@ -164,10 +206,12 @@ class SDTDask:
                     err = getattr(error, key)
                     if err is not None:
                         errors_dict[key].append(err)
+                        # logger.warning(f"{key} : {err}")
                     else:
                         errors_dict[key].append("No Error")
+                        # logger.info(f"{key} : No Error")
             return errors_dict
-        
+
         class DataHandlerData():
             def __init__(self, dh, error):
                 self.dh = dh
@@ -180,8 +224,10 @@ class SDTDask:
                 result = data_plug.get_data(key)
                 dh = DataHandler(result)
                 datahandler = dh
+                logger.info(f"{key} : {str(datahandler)[-19:-1]}")
             except Exception as e:
                 error = str(e)
+                logger.exception(f"{key} : {e}")
             return DataHandlerData(datahandler, error)
 
         for key in KEYS:
@@ -195,7 +241,7 @@ class SDTDask:
             runtimes.append(data.runtime)
             get_data_errors.append(dh_data.error)
             errors.append(data.errors)
-    
+
         # append losses to the report
         df_reports = delayed(generate_report)(reports, losses)
         # generate error dictionary for dataframe
@@ -225,13 +271,17 @@ class SDTDask:
 
     def get_report(self):
         # test if the filepath exist, if not create it
+        time_stamp = strftime("%Y%m%d-%H%M%S")
         if not os.path.exists(self.output_path):
-            print("output path does not exist, creating it...")
+            logger.info("output path does not exist, creating it...")
             os.makedirs(self.output_path)
         # Compute tasks on cluster and save results
-        with performance_report(self.output_path + "/dask-report.html"):
+        logger.info(f"saving reports to {self.output_path}")
+        with performance_report(self.output_path + f"/dask-report"
+                                                   f"-{time_stamp}.html"):
             summary_table = self.client.compute(self.df_reports)
             df = summary_table.result()
-            df.to_csv(self.output_path + "/summary_report.csv")
+            df.to_csv(self.output_path + f"/summary-report-{time_stamp}.csv")
 
+        logger.info("Shutting down Dask Client")
         self.client.shutdown()
