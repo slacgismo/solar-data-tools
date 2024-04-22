563e064dc095758d2865df95bff89a52024472c6
nimishy <114829354+nimishy@users.noreply.github.com>
2024-04-22 15:48:02 -0400

    REMOVE Logging from runner

--- sdt_dask/dask_tool/runner.py
+++ sdt_dask/dask_tool/runner.py
@@ -8,27 +8,12 @@ See the README and tool_demo_SDTDask.ipynb for more information
 """
 import os
 import sys
-import logging
 import pandas as pd
 from time import strftime
 from dask import delayed
 from dask.distributed import performance_report
-from distributed.worker import logger
 from solardatatools import DataHandler
 
-# logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:'
-                               '%(module)s: %(message)s')
-os.makedirs("../logs/", exist_ok=True)
-file_handler = logging.FileHandler('../logs/sdt_dask.log')
-stream_handler = logging.StreamHandler(sys.stdout)
-
-file_handler.setFormatter(formatter)
-stream_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
 
 class Runner:
     """A class to run the SolarDataTools pipeline on a Dask cluster.
@@ -56,7 +41,6 @@ class Runner:
             **kwargs: Optional parameters.
 
         """
-        logger.info(" Setting up SDT dask tool")
 
         reports = []
         runtimes = []
@@ -97,34 +81,25 @@ class Runner:
             run_pipeline_error = None
 
             if datahandler is None:
-                logger.error(f'{str(datahandler)[-19:-1]} : value is None')
                 return None
             try:
                 datahandler.run_pipeline(**kwargs)
-                logger.info(f'{str(datahandler)[-19:-1]} : Runnning Pipeline..')
                 if datahandler.num_days <= 365:
                     datahandler.run_loss_analysis_error = "The length of data is less than or equal to 1 year, loss analysis will fail thus is not performed."
                     datahandler.loss_analysis_report_error = "Loss analysis is not performed"
-                    logger.warning(f"{str(datahandler)[-19:-1]} : Cannot perform loss "
-                                   "analysis as the duration of the datahandler "
-                                   "is less than a year")
                     return datahandler
             except Exception as e:
                 datahandler.run_pipeline_error = str(e)
                 datahandler.run_loss_analysis_error = "Failed because of run_pipeline error"
                 datahandler.run_pipeline_report_error = "Failed because of run_pipeline error"
                 datahandler.loss_analysis_report_error = "Failed because of run_pipeline error"
-                logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
                 return datahandler
 
             try:
                 datahandler.run_loss_factor_analysis()
-                logger.info(f"{str(datahandler)[-19:-1]} : Running loss "
-                            f"factor analysis ...")
             except Exception as e:
                 datahandler.run_loss_analysis_error = str(e)
                 datahandler.loss_analysis_report_error = "Failed because of run_loss_analysis error"
-                logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
 
                 return datahandler
 
@@ -140,46 +115,35 @@ class Runner:
             loss_analysis_report_error = None
             if datahandler is None:
                 errors = DaskErrors("get_data error leading nothing to run", "get_data error leading nothing to analyze", "get_data error leading nothing to report", "get_data error leading nothing to report")
-                logger.error(f"{str(datahandler)[-19:-1]}: Datahandler is none")
+
                 return Data(report, loss_report, runtime, errors)
 
             if hasattr(datahandler, "run_pipeline_error"):
                 run_pipeline_error = datahandler.run_pipeline_error
-                logger.error(f"{str(datahandler)[-19:-1]} : {run_pipeline_error}")
             else:
                 try:
                     report = datahandler.report(return_values=True, verbose=False)
-                    logger.info(f"{str(datahandler)[-19:-1]} : Getting "
-                                f"Datahandler Report ...")
                 except Exception as e:
                     datahandler.run_pipeline_report_error = str(e)
-                    logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
 
             if hasattr(datahandler, "run_loss_analysis_error"):
                 run_loss_analysis_error = datahandler.run_loss_analysis_error
-                logger.error(f"{str(datahandler)[-19:-1]} : {run_loss_analysis_error}")
             else:
                 try:
                     loss_report = datahandler.loss_analysis.report()
-                    logger.info(f"{str(datahandler)[-19:-1]} : Getting Loss "
-                                f"analysis Report ...")
                 except Exception as e:
                     datahandler.loss_analysis_report_error = str(e)
-                    logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
 
             try:
                 runtime = datahandler.total_time
-                logger.info(f"{str(datahandler)[-19:-1]} : Getting Total Time ...")
             except Exception as e:
-                logger.exception(f"{str(datahandler)[-19:-1]}: {e}")
+                print(e)
 
             if hasattr(datahandler, "run_pipeline_report_error"):
                 run_pipeline_report_error = datahandler.run_pipeline_report_error
-                logger.error(f"{str(datahandler)[-19:-1]} : {run_pipeline_report_error}")
 
             if hasattr(datahandler, "loss_analysis_report_error"):
                 loss_analysis_report_error = datahandler.loss_analysis_report_error
-                logger.error(f"{str(datahandler)[-19:-1]} : {loss_analysis_report_error}")
 
             errors = DaskErrors(run_pipeline_error, run_loss_analysis_error, run_pipeline_report_error, loss_analysis_report_error)
             return Data(report, loss_report, runtime, errors)
@@ -206,10 +170,8 @@ class Runner:
                     err = getattr(error, key)
                     if err is not None:
                         errors_dict[key].append(err)
-                        # logger.warning(f"{key} : {err}")
                     else:
                         errors_dict[key].append("No Error")
-                        # logger.info(f"{key} : No Error")
             return errors_dict
 
         class DataHandlerData():
@@ -224,10 +186,8 @@ class Runner:
                 result = data_plug.get_data(key)
                 dh = DataHandler(result)
                 datahandler = dh
-                logger.info(f"{key} : {str(datahandler)[-19:-1]}")
             except Exception as e:
                 error = str(e)
-                logger.exception(f"{key} : {e}")
             return DataHandlerData(datahandler, error)
 
         for key in KEYS:
@@ -270,10 +230,8 @@ class Runner:
         # test if the filepath exist, if not create it
         time_stamp = strftime("%Y%m%d-%H%M%S")
         if not os.path.exists(self.output_path):
-            logger.info("output path does not exist, creating it...")
             os.makedirs(self.output_path)
         # Compute tasks on cluster and save results
-        logger.info(f"saving reports to {self.output_path}")
 
         with performance_report(self.output_path + "/" + f"{time_stamp}-" + dask_report):
             summary_table = self.client.compute(self.df_reports)
@@ -282,5 +240,4 @@ class Runner:
                 df = pd.concat([df, additional_columns], axis=1)
             df.to_csv(self.output_path + "/" + f"{time_stamp}-" + summary_report)
 
-        logger.info("Shutting down Dask Client")
         self.client.shutdown()
