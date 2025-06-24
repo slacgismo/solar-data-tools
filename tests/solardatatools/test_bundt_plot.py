acc15d62da058e815cdc518c2cd00ec79234f5d6
Bennet Meyers <bennet.meyersim@nrel.gov>
2025-06-24 10:20:28 -0700

    new test

---
+++ tests/solardatatools/test_bundt_plot.py
@@ -0,0 +1,13 @@
+import unittest
+import matplotlib.pyplot as plt
+import numpy as np
+from solardatatools.plotting import plot_bundt_cake
+
+class TestBundtPlot(unittest.TestCase):
+    def test_bundt_plot(self):
+        np.random.seed(42)
+        test_data = np.random.rand(365, 100)
+        fig = plot_bundt_cake(test_data)
+
+if __name__ == "__main__":
+    unittest.main()
