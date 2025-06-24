152255f4f1b1333777e6e2effc39b9e21f361b96
Bennet Meyers <bennet.meyersim@nrel.gov>
2025-06-24 10:34:45 -0700

    moved to unittest framework

--- solardatatools/bundt_plotting_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from plotting import plot_bundt_cake
-
-np.random.seed(42)
-test_data = np.random.rand(365, 100)
-fig = plot_bundt_cake(test_data)
-if fig:
-    plt.show()
