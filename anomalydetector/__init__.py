2c3fffa61bf4b87266c2882afe39d38edde5347c
Corentin Servouze <corentintpe1@gmail.com>
2025-09-01 09:02:40 -0700

    display_quantiles

--- anomalydetector/__init__.py
+++ anomalydetector/__init__.py
@@ -1,12 +1,12 @@
 from anomalydetector.multidata_handler import MultiDataHandler
 from anomalydetector.multidata_handler import train_test_split
-from anomalydetector.detector_model import QLinear
+from anomalydetector.detector_model import OutagePipeline
 from anomalydetector.detector_model import save,load
 
 __all__ = [
     "MultiDataHandler",
     "train_test_split",
-    "QLinear",
+    "OutagePipeline",
     "save",
     "load"
 ]
--- anomalydetector/detector_model.py
+++ anomalydetector/detector_model.py
@@ -14,6 +14,9 @@ import matplotlib.pyplot as plt
 import warnings
 import joblib
 import os
+import pandas as pd
+import scipy.stats as stats
+from matplotlib.colors import Normalize
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
@@ -21,11 +24,47 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
 
 
-class QLinear:
+# =================================================================================================
+# ======================================   SKLEARN MODEL   ========================================
+# =================================================================================================
+
+from sklearn.svm import SVC
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+from xgboost import XGBClassifier
+
+
+svm_model = SVC(kernel='rbf')
+xgboost_model = XGBClassifier(n_estimators=100, learning_rate=0.1, random_state=42)
+lda_model = LinearDiscriminantAnalysis()
+logreg_model = LogisticRegression(max_iter=1000)
+qda_model = QuadraticDiscriminantAnalysis()
+ensemble_model =  VotingClassifier(
+            estimators=[
+                ('xgb', xgboost_model),
+                ('svm', svm_model),
+                ('logreg', logreg_model)
+            ],voting='soft')
+
+
+class OutagePipeline:
     def __init__(self,
                  sites,
-                 ndil = None,
-                 target = None
+                 ndil,
+                 target = None,
+                 weight_quantiles = 5,
+                 quantiles = None,
+                 solver_quantiles = 'clarabel',
+                 num_harmonics = [30,3],
+                 nlag = 3,
+                 num_basis = 8,
+                 weight_linear = None,
+                 lambda_range = np.logspace(0, 3, num=10),
+                 num_split = 5,
+                 model_residuals = 'ensemble',
+                 train_size = 0.8
                  ):
         """
         This class contains the object QLinear which is the superposition of a quantile estimation, linear model and 
@@ -43,7 +82,45 @@ class QLinear:
         """
         self.sites = sites
         self.ndil = ndil
-        self.target = target
+        if target is None :
+            self.target = self.sites[0]
+        else :
+            self.target = target
+        self.weight_quantiles = weight_quantiles
+        if quantiles is None :
+            self.quantiles = np.array([0.02, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 0.98])
+        else :
+            self.quantiles = quantiles
+        self.solver_quantiles = solver_quantiles
+        self.num_harmonics = num_harmonics
+        if weight_linear is None :
+            n_features = (len(self.sites)-1) * (2*nlag+1) + 1
+            _default_weight = np.tile(np.arange(1, num_basis+1)**2,n_features)
+            self.weight_linear = _default_weight
+        else :
+            self.weight_linear = weight_linear
+        self.lambda_range = lambda_range
+        self.num_split = num_split
+        
+
+        if isinstance(model_residuals,str) :
+            if model_residuals == "SVM":
+                self.residual_model = svm_model
+            if model_residuals == 'ensemble':
+                self.residual_model = ensemble_model
+            if model_residuals == 'logisticRegression':
+                self.residual_model = logreg_model
+            if model_residuals == 'XGBoost' :
+                self.residual_model = xgboost_model
+            if model_residuals == 'QDA' :
+                self.residual_model = qda_model
+            if model_residuals == 'LDA' :
+                self.residual_model = lda_model
+        else :
+            self.residual_model = model_residuals
+        self.train_size = train_size
+        self.nlag = nlag
+        self.num_basis = num_basis
         self._initialize_attributes()
 
     def _initialize_attributes(self):
@@ -59,11 +136,15 @@ class QLinear:
         self.residuals_failure_test = None
         self.linear_coeff = None
         self.preprocess = None
-        self.residual_model = None
-        self.nlag = None
-        self.num_basis = None
 
-    def fit_quantiles(self,multidata,param = {}):
+    def fit_quantiles(self,
+                      multidata,
+                      weight_quantiles = None,
+                      quantiles = None,
+                      solver_quantiles = None,
+                      num_harmonics = None,
+                      client = None,
+                      ):
         """
         This function fits the first part of the model and compute the intermediate results for the training set
         :param multidata: This object contains all information about the sites, if the data haven't been diluated yet 
@@ -96,25 +177,23 @@ class QLinear:
         elif multidata.target is None and self.target is None :
             raise ValueError('No target given')
         
-        _default_qs = np.array([0.02, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 0.98])
         self.start_train = multidata.common_days[0]
-        weight = param.get("weight_quantiles", 5)
-        quantiles = param.get("quantiles", _default_qs)
-        solver = param.get("solver_quantiles", "clarabel")
-        num_harmonic = param.get("num_harmonics", [30,3])
-        client = param.get("client", None)
+        if weight_quantiles is not None : self.weight_quantiles = weight_quantiles
+        if quantiles is not None : self.quantiles = quantiles
+        if solver_quantiles is not None : self.solver_quantiles = solver_quantiles
+        if num_harmonics is not None : self.num_harmonics = num_harmonics
 
         dict_quantile = {}
         for site in self.sites :
             dict_quantile[site] = SmoothPeriodicQuantiles(
-            num_harmonics=num_harmonic,
+            num_harmonics=self.num_harmonics,
             periods=[self.ndil, 365.24225*self.ndil],
             standing_wave=[True, False],
             trend=False,
-            quantiles=quantiles,
-            weight=weight,
+            quantiles=self.quantiles,
+            weight=self.weight_quantiles,
             problem='sequential',
-            solver=solver,
+            solver=self.solver_quantiles,
             extrapolate='solar'
             )
         self.spqs = dict_quantile
@@ -140,7 +219,13 @@ class QLinear:
         full_failure,_ = full_signal(multidata.common_days,self.start_train,multidata.failure_mat,self.ndil)
         self.quantile_failure = reconstruct_signal(self.spqs[self.target].transform(full_failure),multidata.common_days,self.ndil)
 
-    def fit_linear(self,param = {}):
+    def fit_linear(self,
+                   nlag = None,
+                   num_basis = None,
+                   weight_linear = None,
+                   lambda_range = None,
+                   num_split = None
+                   ):
         """
         This function fit the linear regression part. It creates some lagged features and learn a linear regression where the coefficients
         between each step in a day is driven by a chebyvev basis.
@@ -159,56 +244,80 @@ class QLinear:
         y[np.isnan(y)] = 0
         y_failure[np.isnan(y_failure)] = 0
 
-        nlag = param.get("nlag", 3)
-        num_basis = param.get("num_basis", 8)
-        n_features = (len(self.sites)-1) * (2*nlag+1) + 1
-        _default_weight = np.tile(np.arange(1, num_basis+1)**2,n_features)
-        _default_lbda = np.logspace(0, 3, num=10)
-        weights = param.get("weight_linear", _default_weight)
-        lbda_values = param.get("lambda_range", _default_lbda)
-        n_split = param.get("num_split",5)
-        self.nlag = nlag
-        self.num_basis = num_basis
+        if nlag is not None : self.nlag = nlag
+        if num_basis is not None : self.num_basis = num_basis
+        if weight_linear is not None : self.weight_linear = weight_linear
+        if lambda_range is not None : self.lambda_range = lambda_range
+        if num_split is not None : self.num_split = num_split
 
-        X,y,y_failure = form_xy(X,y,y_failure,nlag)
-        X_flat,y_flat = reshape_for_cheb(X,y,num_basis)
-        _,y_failure_flat = reshape_for_cheb(X,y_failure,num_basis)
-        self.lambda_optimal,self.linear_coeff = optimal_weighted_regression(X_flat,y_flat,weights,lbda_values,n_splits=n_split)
+        X,y,y_failure = form_xy(X,y,y_failure,self.nlag)
+        X_flat,y_flat = reshape_for_cheb(X,y,self.num_basis)
+        _,y_failure_flat = reshape_for_cheb(X,y_failure,self.num_basis)
+        self.lambda_optimal,self.linear_coeff = optimal_weighted_regression(X_flat,
+                                                                            y_flat,
+                                                                            self.weight_linear,
+                                                                            self.lambda_range,
+                                                                            n_splits=self.num_split)
         self.residuals_train = (y_flat-X_flat@self.linear_coeff).reshape(y.shape)
         self.residuals_failure = (y_failure_flat-X_flat@self.linear_coeff).reshape(y_failure.shape)
 
-    def fit_residuals(self,param = {}):
+    def fit_residuals(self,
+                      residual_model = None,
+                      train_size = None
+                      ):
         """
         Finaly this function takes the residuals computed in the prevous step and classify the failure and no-failure case.
         :param param: Dictionnary of every possible parameters for the learning step: model_residuals, train_size.
         :type param: dict
         """
-        _default_model = SVC(kernel='rbf')
-        model = param.get("model_residuals", _default_model)
-        train_size = param.get("train_size", 0.8)
+        if residual_model is not None : self.residual_model = residual_model
+        if train_size is not None : self.train_size = train_size
         X = np.concat([self.residuals_train,self.residuals_failure])
         y = np.concat([[0]*self.residuals_train.shape[0],[1]*self.residuals_failure.shape[0]])
-        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=1-train_size, shuffle=True,random_state=0)
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=1-self.train_size, shuffle=True,random_state=0)
 
         self.preprocess = StandardScaler()
         self.preprocess.fit(X_train)
         X_train = self.preprocess.transform(X_train)
         X_val = self.preprocess.transform(X_val)
 
-        model.fit(X_train,y_train)
-        self.residual_model = model
+        self.residual_model.fit(X_train,y_train)
 
-        return {"accuracy_train" : accuracy_score(y_train,model.predict(X_train)),
-                "f1_score_train" : f1_score(y_train,model.predict(X_train)),
-                "accuracy_val" : accuracy_score(y_val,model.predict(X_val)),
-                "f1_score_val" : f1_score(y_val,model.predict(X_val))
+        return {"accuracy_train" : accuracy_score(y_train,self.residual_model.predict(X_train)),
+                "f1_score_train" : f1_score(y_train,self.residual_model.predict(X_train)),
+                "accuracy_val" : accuracy_score(y_val,self.residual_model.predict(X_val)),
+                "f1_score_val" : f1_score(y_val,self.residual_model.predict(X_val))
                 }
 
-    def fit(self,mutlidata,param = {}):
-        self.fit_quantiles(mutlidata,param)
-        self.fit_linear(param)
-        return self.fit_residuals(param)
-
+    def fit(self,
+            mutlidata,
+            weight_quantiles = None,
+            quantiles = None,
+            solver_quantiles = None,
+            num_harmonics = None,
+            client = None,
+            nlag = None,
+            num_basis = None,
+            weight_linear = None,
+            lambda_range = None,
+            num_split = None,
+            residual_model = None,
+            train_size = None
+            ):
+        self.fit_quantiles(mutlidata,
+                           weight_quantiles=weight_quantiles,
+                           quantiles=quantiles,
+                           solver_quantiles=solver_quantiles,
+                           num_harmonics=num_harmonics,
+                           client=client)
+        self.fit_linear(nlag = nlag,
+                        num_basis=num_basis,
+                        weight_linear=weight_linear,
+                        lambda_range=lambda_range,
+                        num_split=num_split)
+        return self.fit_residuals(residual_model=residual_model,
+                                  train_size=train_size)
+    
     def predict_quantiles(self,multidata):
 
         """
@@ -396,6 +505,60 @@ class QLinear:
         coeffs = np.dot(c, theta_reshaped.T)
         return coeffs
 
+    def display_quantiles(self, site=None, num_day=7, idx=0, print_train = True):
+        if site is None:
+            site = self.target
+        fig, axes = plt.subplots(1, 2, figsize=(18, 5))
+
+        cmap1 = plt.get_cmap('coolwarm')
+        fq = self.spqs[site].fit_quantiles[idx*self.ndil:(idx+num_day)*self.ndil, :]
+        colors1 = cmap1(np.linspace(0, 1, fq.shape[1]))
+
+        for j in range(fq.shape[1]):
+            axes[0].plot(fq[:, j], color=colors1[j], alpha=0.5, label=self.quantiles[j])
+
+
+        xticks_pos = np.arange(0, fq.shape[0], self.ndil)
+
+        start_date = pd.to_datetime(self.start_train) + pd.to_timedelta(idx, unit="D")
+        xticks_labels = [(start_date + pd.Timedelta(days=i)).strftime("%Y-%m-%d") for i in range(num_day)]
+
+        axes[0].set_xticks(xticks_pos)
+        axes[0].set_xticklabels(xticks_labels, rotation=45)
+        axes[0].set_title("Quantile functions")
+        axes[0].legend()
+
+        if print_train :
+            raw = self.quantile_train[site]  
+        else :
+            raw = self.quantile_test[site]           
+        data = raw.T.flatten()                       
+
+        osm, _ = stats.probplot(data, dist="norm")
+        idxs = np.arange(len(data))
+        cmap2 = plt.get_cmap('plasma')  
+        norm = Normalize(vmin=0, vmax=self.ndil - 1)
+        colors = cmap2(norm(idxs % self.ndil))
+
+
+        axes[1].scatter(osm[0], osm[1], c=colors, alpha=0.7, s=12)
+
+        sm = plt.cm.ScalarMappable(cmap=cmap2, norm=norm)
+        sm.set_array([])
+        cbar = plt.colorbar(sm, ax=axes[1])
+        cbar.set_label(f"Index within time window (0 → {self.ndil - 1})")
+
+
+        min_val = min(osm[0].min(), osm[1].min())
+        max_val = max(osm[0].max(), osm[1].max())
+        axes[1].plot([min_val, max_val], [min_val, max_val], 'r--', lw=1)
+
+        axes[1].set_title("QQ-plot of transformed data")
+        axes[1].legend()
+
+        plt.tight_layout()
+        plt.show()
+
 
 def save(file, obj): 
     """
@@ -416,7 +579,7 @@ def save(file, obj):
     # Save the object with joblib using gzip compression
     joblib.dump(obj, file, compress=('gzip', 3))
 
-def load(file) -> QLinear:
+def load(file) -> OutagePipeline:
     """
     Load a QLinear object from a file using joblib.
     
